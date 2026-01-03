91a794006d8679f6902c175cb04a30d4b33b651a
Sebastian Jorge <sebastianjorge@fibertel.com.ar>
2026-01-02 22:14:30 -0300

    Implement initial project structure and integrate login/register functionality from legacy project.

--- routes/api.php
+++ routes/api.php
@@ -1,54 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\profile\PasswordController;
 use App\Http\Controllers\Api\transaction\regTransactionController;
 use App\Http\Controllers\Api\profile\profileController;
 use App\Http\Controllers\Api\transaction\delTransactionController;
 
-
-
-
-use App\Http\Requests\RegistationRequest;
-
-
 # Home 
 Route::middleware(['auth:api'])->group(function () {
     Route::get('/profile/home', [profileController::class, 'home']);
 });
 
-
-
-//transactions routes
-
+# Transactions routes
 Route::middleware(['auth:api'])->group(function () {
     Route::post('/transaction/create', [regTransactionController::class, 'createTransaction']);
-});
-
-Route::middleware(['auth:api'])->group(function(){
     Route::delete('/transaction/delete', [delTransactionController::class, 'deleteTransaction']);
 });
 
-//Route::middleware(['auth:api'])->group(function(){
-//    Route::update('/transaction/update', [updateTransactionController::class, 'updateTransaction']);
-//});
+
+
+# Transactions routes
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('/crateBook/create', [regBookController::class, 'createBook']);
+    Route::delete('/Book/delete', [delBookController::class, 'deleteBook']);
+});
 
 
 
-#Auth Routes
+
+
+
+
+
+
+# Auth Routes
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/verify_user_email', [AuthController::class, 'verifyUserEmail']);
 Route::post('/auth/resend_verification_link', [AuthController::class, 'ResendEmailVerificationLink']);
 
-
-
-
-
-
-Route::middleware(['auth'])->group(function(){
+# Password Routes
+Route::middleware(['auth'])->group(function () {
     Route::post('change_password', [PasswordController::class, 'changePassword']);
-
 });
